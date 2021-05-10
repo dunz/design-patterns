383832906ab0d4f7cde573bde0e1813853176e34
dunz <zergcake@gmail.com>
2021-05-10 23:33:40 +0900

    ph-pages 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from './logo.svg'
 import './App.css'
 import {Singleton} from "./Singleton";
 
 function App() {
+  const [count, setCount] = useState(0)
+
   const ins1 = new Singleton();
   const ins2 = new Singleton();
   const ins3 = new Singleton();
@@ -13,6 +16,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
         <p>
           <a
             className="App-link"
