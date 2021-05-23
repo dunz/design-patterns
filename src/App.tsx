75a1e1c4e95eadf2272c6b77602f5f877e249367
dunz <zergcake@gmail.com>
2021-05-24 00:11:11 +0900

    add eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,44 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import {Singleton} from "./Singleton";
-import example from './example.module.css'
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Singleton } from './Singleton';
+import example from './example.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  const ins1 = new Singleton();
-  const ins2 = new Singleton();
-  const ins3 = new Singleton();
+    const ins1 = new Singleton();
+    const ins2 = new Singleton();
+    const ins3 = new Singleton();
+    var asdf = 'asdf';
 
-  console.log('ins1 === ins2', ins1 === ins2);
-  console.log('ins1 === ins3', ins1 === ins3);
-  console.log(example);
+    console.log('ins1 === ins2', ins1 === ins2);
+    console.log('ins1 === ins3', ins1 === ins3);
+    console.log(example);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello Vite + React!</p>
+                <p>
+                    <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
+                </p>
+                <p>
+                    Edit <code>App.tsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a className="App-link" href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener noreferrer">
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    );
 }
 
-export default App
+export default App;
